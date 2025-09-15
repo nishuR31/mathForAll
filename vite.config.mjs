96703a5501e02523069945ac054459f516860ab4
nishuR31 <nishangithub@gmail.com>
2025-09-16 01:39:03 +0530

    api issue

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,13 +8,14 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: "dist",
+    outDir: "build",
+    chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
-  // server: {
-  //   port: "4028",
-  //   host: "0.0.0.0",
-  //   strictPort: true,
-  //   allowedHosts: [".amazonaws.com", ".builtwithrocket.new"],
-  // },
+  server: {
+    port: "4028",
+    host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: [".amazonaws.com", ".builtwithrocket.new"],
+  },
 });
