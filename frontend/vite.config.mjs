1942c171df3dca5ec9545eb999df45cb47bc3add
nishuR31 <nishangithub@gmail.com>
2025-09-28 19:09:12 +0530

    now

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
+export default defineConfig({
+  base: "/",
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
+  plugins: [tsconfigPaths(), react(), tagger()],
+  server: {
+    port: "5173",
+  },
+});
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -24,17 +34,6 @@ import tagger from "@dhiwise/component-tagger";
 //     },
 //   },
 // });
-
-export default defineConfig({
-  base: "/",
-  build: {
-    chunkSizeWarningLimit: 2000,
-  },
-  plugins: [tsconfigPaths(), react(), tagger()],
-  server: {
-    port: "5173",
-  },
-});
 
 // import { defineConfig, loadEnv } from "vite";
 // import react from "@vitejs/plugin-react";
